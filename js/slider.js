400746ee3523ec21dfe30898ea31e99e17392a5a
Zidane Fauzan <153303119+zidanefzn@users.noreply.github.com>
2024-07-21 06:41:12 +0700

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -3,14 +3,12 @@ const swiper = new Swiper('.slider-wrapper', {
     grabCursor: true,
     spaceBetween: 30,
   
-    // If we need pagination
     pagination: {
       el: '.swiper-pagination',
       clickable: true,
       dynamicBullets: true
     },
   
-    // Navigation arrows
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
